6981e6465567f5e027b4282f914612994d30ac1a
jordanhill <jordan.m.hill@icloud.com>
2016-08-06 14:49:24 -0500

    Create mp3ConvertWav.sh
    #####################################
    This script will take the input from the user to specify a folder of .wav files to convert to .mp3. It then will perform the conversion and place the files into a specified directory. 
    
    Future Version Ideas: 
    Take input from user for MP3 Quality
    Make it look sleeker, fix echo formatting etc.

---
+++ mp3ConvertWav.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+###################################
+# Wav to MP3 Bash Script v1.0     #
+# By: Jordan Hill                 #
+# August 2016                     #
+###################################
+clear # Clear the Screen
+#Setup ANSI Color Codes
+RED='\033[0;31m'
+DEFAULT='[0m'
+ # Get Directory To Convert
+echo -e "${RED}		Please input the directory that you wish to convert:\e[0m" 
+read convert
+# If filenames contain whitespace, replace with _ 
+for f in $convert/* ; do mv "$f" "${f// /_}" 2>/dev/null ; done
+      # Get Directory to Put Converted Files In 
+echo -e "${RED}	Please input the directory that you wish to place the converted files:\e[0m"
+read converted
+mkdir -p $converted
+      # Begin Conversion
+echo "		Please wait..."
+echo "		Converting $convert"
+#Convert direcotry of wavs into mp3s
+for i in $convert/*.wav 
+	do ffmpeg -loglevel 0 -i "$i" -c:a libmp3lame -b:a 320k "$i.mp3" #encode files
+	mv $convert/*.mp3 $converted #Move files into converted folder
+done
+#File names end up becoming .wav.mp3 so we need to strip the .wav part.
+for file in $converted/*.wav.mp3
+	 do mv "$file" "${file//.wav}" #Strip .wav from filename
+done
+sleep 2
+echo "		All files have been converted!"  # All Files Converted
+echo "		Files have been placed in $converted"
+#Show all files in $converted directory
+ls -lah $converted 
+sleep 5
+echo "Goodbye.
